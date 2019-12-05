976a00528a5d0de50fdd3564a97821c7b51353d3
huyixiang <huyixiang@olayc.cn>
2019-12-05 11:00:40 +0800

    1、换行样式调整

--- app/src/main/java/com/ole/common_toast/MainActivity.java
+++ app/src/main/java/com/ole/common_toast/MainActivity.java
@@ -14,7 +14,7 @@ import com.ole.libtoast.T;
 public class MainActivity extends Activity implements View.OnClickListener{
 
 
-    private Button one,one1,one2,one3,one4;
+    private Button one,one1,one2,one3,one4,one5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +27,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
         one2=findViewById(R.id.one2);
         one3=findViewById(R.id.one3);
         one4=findViewById(R.id.one4);
+        one5=findViewById(R.id.one5);
         one1.setOnClickListener(this);
         one2.setOnClickListener(this);
         one3.setOnClickListener(this);
         one4.setOnClickListener(this);
+        one5.setOnClickListener(this);
 
           }
 
@@ -52,6 +54,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 break;
             case R.id.one4:
                 T.showCustomView(this,"123123123");
+                break;
+            case R.id.one5:
+                T.showWrongCustomImage(this,"欢迎来到我的大世界\n一起疯狂");
                 break;
         }
     }
